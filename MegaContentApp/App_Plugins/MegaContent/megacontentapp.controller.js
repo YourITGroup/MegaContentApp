4b67ff2bc0ea2d803d4122a1bce196ad130bdf5d
Robert Foster <robert@youritgroup.com.au>
2019-01-22 14:45:36 +1000

    Add project files.

---
+++ MegaContentApp/App_Plugins/MegaContent/megacontentapp.controller.js
@@ -0,0 +1,29 @@
+﻿function megaContentAppController($scope, editorState, userService, umbRequestHelper, $log, $http) {
+
+    var vm = this;
+    vm.CurrentNodeId = editorState.current.id;
+    vm.CurrentNodeAlias = editorState.current.contentTypeAlias;
+    vm.Counter = 0;
+
+    vm.UpdateCounter = function () {
+        vm.Counter++;
+
+        // we can add a badge to the app by defining $scope.model.badge - options are:
+        // - count: Optional. Adds a number to the badge. 
+        // - type: Optional. Sets the "severity" color of the badge - use "warning" or "alert".
+        $scope.model.badge = {
+            count: vm.Counter,
+            type: vm.Counter > 9 ? "alert" : vm.Counter > 4 ? "warning" : null
+        };
+    };
+
+    function init() {
+        var user = userService.getCurrentUser().then(function (user) {
+            console.log(user);
+            vm.UserName = user.name;
+        });
+    }
+
+    init();
+}
+angular.module("umbraco").controller("ContentApps.MegaContentApp", megaContentAppController);
---
+++ README.md
@@ -0,0 +1,13 @@
+# Mega Content App
+
+Sample Content App solution
+
+## Credentials
+
+The default (Umbraco backoffice) login for the Umbraco site is:
+
+| Username | Password |
+| -------- | -------- |
+| admin@youritteam.com | H1ghlyS3cure! |
+
+You can log in at http://localhost:38903/umbraco#/content
